c1938d52cbf1b77b788d235d60d3847cb60c8525
BhattYashesh <yasheshbhatt777@gmail.com>
2024-11-28 18:28:52 +0530

    code

---
+++ day10/lastoccurence.js
@@ -0,0 +1,29 @@
+let arr= [1,2,2,4,5,6,7,8];
+let target=2;
+
+let low=0;
+let high=arr.length-1;
+let ans = -1;
+
+while(low<=high)
+    {
+         let mid= Math.floor((low+high)/2);
+   
+           if(arr[mid]==target)
+           {
+               ans=mid;
+             low = mid + 1;
+           }
+   
+           if(arr[mid]<target)
+           {
+               low = mid + 1;
+           }
+           else if(arr[mid]>target)
+           {
+               high = mid - 1;
+           }
+    }
+   
+    console.log(ans);
+    
---
+++ day10/target.js
@@ -0,0 +1,26 @@
+let arr= [1,2,3,4,5,6,7,8];
+let target=3;
+
+let low=0;
+let high=arr.length-1;
+
+
+   while(low<=high)
+   {
+      let mid = Math.floor((low+high)/2);
+
+       if(arr[mid]==target)
+       {
+         console.log(mid);
+         break;
+       }
+
+        if(arr[mid]<target)
+        {
+            low = mid+1;
+        }
+        else if(arr[mid]>target)
+        {
+            high=mid+1;
+        }
+   }
